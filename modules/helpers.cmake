9f5f2ab4c8cb5a77c216329706faa3710fe683a6
kj16609 <bigskyman02@gmail.com>
2025-06-26 16:33:38 -0400

    Remove cxx_modules flag again from targets

--- modules/helpers.cmake
+++ modules/helpers.cmake
@@ -23,7 +23,6 @@ function(ConfigureFGLTarget NAME SRC_DIR INCLUDE_DIR)
 		target_compile_definitions(${NAME} PUBLIC "-DFGL_STRICT_WARNINGS=1")
 	endif ()
 
-	set_target_properties(${NAME} PROPERTIES CXX_MODULE_STD 1)
 	target_compile_features(${NAME} PUBLIC cxx_std_23)
 endfunction()
 
